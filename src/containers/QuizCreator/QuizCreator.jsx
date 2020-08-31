3d45937043498bfad59a1d2a21774f624f8e2b73
avshultsev <splendide.mendax.gamer.z@gmail.com>
2020-08-31 16:32:18 +0300

    QuizCreator part 2
    Select common component added, functionality of the inputs expanded

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from './QuizCreator.module.css';
 import Input from '../../UI/Input/Input';
 import Button from '../../UI/Button/Button';
+import Select from '../../components/Select/Select';
 import {createControl} from '../../form/formFramework';
 
 const createOptionControl = number => {
@@ -28,6 +29,7 @@ const createFormControl = () => {
 export default class QuizCreator extends Component {
   state = {
     isFromValid: false,
+    correctAnswerId: 1,
     quiz: [],
     formControls: createFormControl()
   }
@@ -44,28 +46,23 @@ export default class QuizCreator extends Component {
     event.preventDefault();
   }
 
-  renderInputs() {
-    return Object.keys(this.state.formControls).map((controlName, index) => {
-      const control = this.state.formControls[controlName];
+  inputHandler = (value, controlName) => {
+    // console.log(id, event.target.value);
+    
+    const formControlsCopy = {...this.state.formControls};
+    let control = {...formControlsCopy[controlName]};
+    
+    control.value = value;
+    formControlsCopy[controlName] = control;
+    
+    this.setState({
+      formControls: formControlsCopy
+    })
+  }
 
-      return (
-        <React.Fragment>
-          <Input 
-            key={index.toString()}
-            label={control.label}               //config object
-            errorMessage={control.errorMessage} //config object
-            value={control.value}               //return of createControl()
-            type={control.type}
-            
-            valid={control.valid}               //return of createControl()
-            touched={control.touched}           //return of createControl()
-            required={control.required}
-
-            inputHandler={event => this.inputHandler(event)}
-          />
-          {index === 0 && <hr />}
-        </React.Fragment>
-      )
+  selectChangeHandler = value => {
+    this.setState({
+      correctAnswerId: +value
     })
   }
 
@@ -76,8 +73,45 @@ export default class QuizCreator extends Component {
           <h1>Create your own quiz!</h1>
           <form onSubmit={this.submitHandler}>
             {
-              this.renderInputs()
+              Object.keys(this.state.formControls).map((controlName, index) => {
+                const control = this.state.formControls[controlName];
+
+                return (
+                  <React.Fragment key={index}>
+                    <Input
+                      key={index}
+                      label={control.label}               //config object
+                      errorMessage={control.errorMessage} //config object
+                      id={control.id || 0}                //config object
+                      value={control.value}               //return of createControl()
+                      type={control.type}
+
+                      isCorrect={this.state.correctAnswerId === control.id}
+                      
+                      valid={control.valid}               //return of createControl()
+                      touched={control.touched}           //return of createControl()
+                      required={control.required}
+          
+                      inputHandler={event => this.inputHandler(event.target.value, controlName)}
+                    />
+                    {index === 0 && <hr />}
+                  </React.Fragment>
+                )
+              })
             }
+            
+            <Select 
+              label='Choose correct answer'
+              selectedValue={this.state.correctAnswerId}
+              options={[
+                {text: 1, value: 1},
+                {text: 2, value: 2},
+                {text: 3, value: 3},
+                {text: 4, value: 4}
+              ]}
+              selectChangeHandler={event => this.selectChangeHandler(event.target.value)}
+            />
+
           </form>
           <div className={styles.buttonSection}>
             <Button 
